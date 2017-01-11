39647ab0c24735705cfd2e3b3c0e67319b901db4
Detelin Zlatev <detelin.zlatev@cayetanogaming.com>
2017-01-11 16:18:28 +0200

    Added few more improvements

--- src/pages/clients/saloons/saloons.ts
+++ src/pages/clients/saloons/saloons.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import {SaloonPage} from '../saloon/saloon'
+import {SearchPage} from '../search/search'
+
 @Component({
   selector: 'page-saloons',
   templateUrl: 'saloons.html'
@@ -10,6 +13,18 @@ export class SaloonsPage {
 
   constructor(public navCtrl: NavController) {
     
+  }
+
+  goToSaloon() {
+    //push another page onto the history stack
+    //causing the nav controller to animate the new page in
+    this.navCtrl.push(SaloonPage);
+  }
+
+  goToSearch() {
+    //push another page onto the history stack
+    //causing the nav controller to animate the new page in
+    this.navCtrl.push(SearchPage);
   }
 
 }
--- src/pages/owners/edit/edit.ts
+++ src/pages/owners/edit/edit.ts
@@ -12,4 +12,8 @@ export class EditPage {
     
   }
 
+  close() {
+      this.navCtrl.pop();
+  }
+
 }
