1d19950081862893e6409b41737fb49637e19507
didoya <detelin.zlatev@gmail.com>
2017-01-19 23:19:01 +0200

    Added first REST request

--- src/pages/clients/search/search.ts
+++ src/pages/clients/search/search.ts
@@ -2,22 +2,37 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
-import {SaloonsPage} from '../saloons/saloons'
+import {SaloonsPage} from '../saloons/saloons';
+
+import {MetadataService} from '../../../providers/metadata-service';
 
 @Component({
   selector: 'page-search',
-  templateUrl: 'search.html'
+  templateUrl: 'search.html',
+  providers: [MetadataService]
 })
 export class SearchPage {
 
-  constructor(public navCtrl: NavController) {
-    
+  public categories: any;
+
+  public cities: any;
+
+  constructor(public navCtrl: NavController, public metadataService: MetadataService) {
+      this.loadMetadata();
   }
 
   goToSaloons() {
     //push another page onto the history stack
     //causing the nav controller to animate the new page in
     this.navCtrl.push(SaloonsPage);
+  }
+
+  loadMetadata() {
+      this.metadataService.load()
+      .then(data => {
+        this.categories = data.categories;
+        this.cities = data.cities;    
+      });
   }
 
 }
