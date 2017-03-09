4735837bef79a5b617a8526296838f380d680e2b
didoya <detelin.zlatev@gmail.com>
2017-03-09 17:21:56 +0200

    Fixed

--- src/pages/clients/saloon/saloon.ts
+++ src/pages/clients/saloon/saloon.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import {CallNumber} from 'ionic-native';
+
 import { NavController, NavParams } from 'ionic-angular';
 
 import {AppSettings} from '../../../appSettings';
@@ -16,6 +18,10 @@ export class SaloonPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
       this.imagesPath = AppSettings.API_ENDPOINT + 'img/upload/';
       this.saloon = this.navParams.get('saloon');
+  }
+
+  call() {
+    CallNumber.callNumber(this.saloon.phone_1, true);
   }
 
 }
